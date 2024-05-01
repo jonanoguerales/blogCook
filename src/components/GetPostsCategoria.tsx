f8d4a06431fcc8eba15e3f9bde3964709c3ac1c7
jonanoguerales <jonafutb@gmail.com>
2024-05-01 20:53:37 +0200

    categoria

--- src/components/GetPostsCategoria.tsx
+++ src/components/GetPostsCategoria.tsx
@@ -5,38 +5,37 @@ import { CardPost } from "./CardPost";
 import { getPostsCategorias } from "@/lib/api";
 
 export function GetPostsCategoria({ category }: { category: string }) {
-    const [posts, setPosts] = useState<Posts[]>([]);
+  const [posts, setPosts] = useState<Posts[]>([]);
 
-    useEffect(() => {
-        async function fetchPosts() {
-            try {
-                const data = await getPostsCategorias(category);
-                setPosts(data);
-            } catch (error) {
-                console.error(error);
-                throw error;
-            }
-        }
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const data = await getPostsCategorias(category);
+        setPosts(data);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    }
 
-        fetchPosts();
-    }, []);
+    fetchPosts();
+  }, [category]);
 
-    return (
-        <>
-            {posts.map((post: Posts) => (
-                <CardPost
-                    key={post._id}
-                    id={post._id}
-                    titulo={post.title}
-                    parrafo={post.desc}
-                    img={post.photo}
-                    categoria={post.categories}
-                    autor={post.username}
-                    fecha={post.createdAt}
-                    bg={post.bg}
-                />
-            ))}
-        </>
-    );
+  return (
+    <>
+      {posts.map((post: Posts) => (
+        <CardPost
+          key={post._id}
+          id={post._id}
+          titulo={post.title}
+          parrafo={post.desc}
+          img={post.photo}
+          categoria={post.categories}
+          autor={post.username}
+          fecha={post.createdAt}
+          bg={post.bg}
+        />
+      ))}
+    </>
+  );
 }
-
